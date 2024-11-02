ce1aa5db52143013d52f194d3b09c61d196bd4d2
vladislavzabrodskyi <vladyslav.zabrodskyi@fordewind.io>
2024-11-02 11:56:21 +0200

    Zabrodskyi Implementation

---
+++ src/SERC20.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.13;
+
+import "solmate/src/tokens/ERC20.sol";
+import "solmate/src/auth/Owned.sol";
+
+contract SERC20 is ERC20, Owned {
+    constructor() ERC20("SellerERC20", "SERC20", 18) Owned(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
