52df05a835a4b5ff96e0cfa0204997c9fe5e2fc6
vladislavzabrodskyi <vladyslav.zabrodskyi@fordewind.io>
2024-11-02 12:48:59 +0200

    change to ownable?

--- src/BERC20.sol
+++ src/BERC20.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.13;
 
 import "solmate/src/tokens/ERC20.sol";
-import "solmate/src/auth/Owned.sol";
+import {Ownable} from "v4-core/lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract BERC20 is ERC20, Owned {
-    constructor() ERC20("BuyerERC20", "BERC20", 18) Owned(msg.sender) {}
+contract BERC20 is ERC20, Ownable {
+    constructor() ERC20("BuyerERC20", "BERC20", 18) Ownable(msg.sender){}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
--- src/SERC20.sol
+++ src/SERC20.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.13;
 
 import "solmate/src/tokens/ERC20.sol";
-import "solmate/src/auth/Owned.sol";
+import {Ownable} from "v4-core/lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract SERC20 is ERC20, Owned {
-    constructor() ERC20("SellerERC20", "SERC20", 18) Owned(msg.sender) {}
+contract SERC20 is ERC20, Ownable {
+    constructor() ERC20("SellerERC20", "SERC20", 18) Ownable(msg.sender){}
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
