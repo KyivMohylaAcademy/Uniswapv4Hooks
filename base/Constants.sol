7b6f408286f7574eb07cab8a6809450ddec20622
Ihor Kubrysh <55651856+kubrysh@users.noreply.github.com>
2024-11-26 01:20:25 +0200

    Implement deployment script

---
+++ base/Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
+
+/// @notice Shared constants used in scripts
+contract Constants {
+    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
+
+    /// @dev populated with default anvil addresses
+    IPoolManager constant POOLMANAGER = IPoolManager(address(0x8C4BcBE6b9eF47855f97E675296FA3F6fafa5F1A));
+}
